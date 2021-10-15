db8abe170807572982b63051b0ebc15ccdc6bd0f
riki95 <ricba1995@gmail.com>
2021-10-15 19:28:07 +0200

    added logs and a bit of logic

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ var robot = require("robotjs");
 
 async function startKeySender() {
 
+    for (let s in 5) {
+        console.log(`Starting in ${s}`);
+        await sleep(1000);
+    }
+
+    console.log('Loop starting');
+    let key = 'space';
+
     while (true) {
         await sleep(1000);
-        robot.keyTap("space");
+        console.log(`Pressing ${key}`)
+        robot.keyTap(key);
     }
 }
 
