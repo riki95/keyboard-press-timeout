04114308d5085f505b44b2bf1a746f9420a83dc1
riki95 <ricba1995@gmail.com>
2021-10-15 21:15:52 +0200

    added switch logic

--- Logics/logicExe.js
+++ Logics/logicExe.js
@@ -9,7 +9,15 @@ const logic_KeyPress = require('./InfiniteKeyPress');
 function run(args) {
     let func = _.first(args);
 
-    console.log(func);
+    switch(func) {
+        case 'hearthstone':
+            logic_Heartstone.HSloop();
+        case 'keypress':
+            logic_KeyPress.infiniteLoop();
+        default:
+            console.log('Not a valid command. Please retry');
+            return;
+    }
 
 }
 
