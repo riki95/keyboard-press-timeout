d7c36ea28d25c2574b5d6ac8b431dd54486add4f
riki95 <ricba1995@gmail.com>
2021-10-15 19:39:20 +0200

    added logic for hearthstone card packs opening

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ function sleep(ms) {
 
 var robot = require("robotjs");
 
+async function HSloop() {
+    let key = 'space';
+
+    while(true) {
+        console.log('Opening Card Pack')
+        robot.keyTap(key);
+        await sleep(3000);
+
+        console.log('Unveiling cards');
+        let card_n = 5;
+        for (let card_i = 0; card_i < card_n; card_i++) {
+            robot.keyTap(key);
+        }
+
+        await sleep(2000);
+        console.log('Closing Pack');
+        robot.keyTap(key);
+    }
+}
+
+async function infiniteLoop() {
+    let key = 'space';
+
+    while (true) {
+        await sleep(1000);
+        console.log(`Pressing ${key}`)
+        robot.keyTap(key);
+    }
+}
+
 async function startKeySender() {
 
     let seconds = 5;
@@ -16,13 +46,9 @@ async function startKeySender() {
     }
 
     console.log('Loop starting');
-    let key = 'space';
 
-    while (true) {
-        await sleep(1000);
-        console.log(`Pressing ${key}`)
-        robot.keyTap(key);
-    }
+    HSloop();
+    //infiniteLoop();
 }
 
 startKeySender();
