05629d0f079b8a69edb03181d350e2c256466eeb
riki95 <ricba1995@gmail.com>
2021-10-16 18:27:25 +0200

    move wait for start

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const helper = require('./Utils/helper');
 const utils = require('./Utils/utils');
 const logic = require('./Logics/logicExe');
 
-async function waitForStart() {
+
+
+async function startKeySender(args) {
+
+    //WAIT before start
     let seconds = 5;
     for (let s = seconds; s >= 0; s--) {
         console.log(`Starting in ${s}`);
         await utils.sleep(1000);
     }
-}
-
-
-async function startKeySender(args) {
-    waitForStart();
+    
     
 
     if (args.length === 0) {
